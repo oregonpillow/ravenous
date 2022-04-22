863b196a42eeda436a680129b86c319e7fe42e7a
debian <oregonpillow@gmail.com>
2022-04-22 17:00:59 +0200

    part 2 complete

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,42 +1,40 @@
 import React from 'react';
 import './App.css';
-//import logo from './logo.svg';
-import BusinessList from '../BusinessList/BusinessList'
-import SearchBar from '../SearchBar/SearchBar'
+
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
 
 const business = {
-  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
   name: 'MarginOtto Pizzeria',
   address: '1010 Paddington Way',
-  city: 'Flavortown',
+  city: 'Bordertown',
   state: 'NY',
   zipCode: '10101',
   category: 'Italian',
   rating: 4.5,
   reviewCount: 90
-  }
-
+};
 
 const businesses = [
-business,
-business,
-business,
-business,
-business,
-business,
-]
-
-
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+];
 
 class App extends React.Component {
   render() {
-  return (
-  <div className="App">
-    <h1>ravenous</h1>
-    <SearchBar />
-    <BusinessList businesses={businesses} /> 
-  </div>
-  )};
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList businesses={businesses} />
+      </div>
+    );
+  }
 }
 
 export default App;
